52fcd4d590773f85e789d9546e1ec146c41ef2cd
khoaminh1311 <minhkhoa131103@gmail.com>
2026-06-12 16:44:59 +0700

    feat: dev B cap nhat thue 10 phan tram

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
 def tinh_thue_thu_nhap(thu_nhap):
-    # Dev A áp dụng giảm trừ 4 trieu va thue 5%
-    thu_nhap_tinh_thue = thu_nhap - 4000000
-    return max(0, thu_nhap_tinh_thue * 0.05)
+   return thu_nhap * 0.1
